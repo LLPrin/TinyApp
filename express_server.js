492cfba5ee4433b33abaf2915ae9e58bafce0998
LLPrin <LLPrin@users.noreply.github.com>
2016-11-02 20:56:19 +0000

    working for me

--- express_server.js
+++ express_server.js
@@ -3,22 +3,27 @@ const app = express();
 const PORT = process.env.PORT || 8080; // default port 8080
 const methodOverride = require("method-override") //gives express ability to allow server to respond to nons differently
 
-const bodyParser = require("body-parser"); //allows use to grab shit from body
-const cookieParser = require('cookie-parser'); //
+const bodyParser = require("body-parser"); //allows user to grab shit from body
+// const cookieParser = require('cookie-parser'); //switched from cookie-parser to cookie-session
 const bcrypt = require('bcrypt');
 const cookieSession = require('cookie-session')
 
 app.use(bodyParser.urlencoded({ extended: true }));
 // app.use connects body parser to express
 app.set("view engine", "ejs");
-app.use(cookieSession())
 
+// Cookie Sesssion uses REQ.session NOT RES.session
+app.use(cookieSession({
+  name: 'session',
+  keys: ['key1', 'key2']
+}));
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
 
 
+// HEY, YOU! YEAH, YOU! NO USERS WHEN SERVER FIRES UP, NEED TO REGISTER!
 //-------------------------------------------DATA
 
 var urlDatabase = {
@@ -30,15 +35,14 @@ var urlDatabase = {
             }
 };
 var users = {};
-// users['123456'] = { username: 'lydia', email: 'lydia@email.com', password: 'pw0'};
-//-------------------------------------------ROUTES
-//GET DATA
+
+//-------------------------------------------BASIC ROUTES
 
 app.get("/urls", (req, res) => {
-  if(req.cookies !== {}){
+  if(req.session !== null){
     let templateVars = { urls: urlDatabase,
-    userid: req.cookies.userid,
-    username: req.cookies.username
+    userid: req.session.user_id,
+    username: req.session.username
     };
     res.render("urls_index", templateVars);
   } else {
@@ -48,140 +52,98 @@ app.get("/urls", (req, res) => {
 
 app.get("/", (req, res) => {
   //Check if cookie present, not > login
-  if (req.cookies) {
-    debugger;
+  if (req.session !== null) {
     res.redirect("/urls")
   } else {
     res.redirect("/login");
-    debugger;
   }
 });
 
-app.get("/urls.json", (req, res) => {
-  let templateVars = { urls: urlDatabase,
-  userid: req.cookies.userid};
-  // input: req.body.username};
-  res.render("urls_index", templateVars);
-});
-
-
 // -----------------------------------------------CREATE URL
 app.get("/urls/create", (req, res) => {
-   if(req.cookies !== {}){
+   if(req.session !== null){
     let templateVars = { urls: urlDatabase,
-    userid: req.cookies.userid,
-    username: req.cookies.username
+    userid: req.session.user_id,
+    username: req.session.username
     };
     res.render("urls_new", templateVars);
   } else {
-    debugger;
     res.redirect("/login");
   }
 });
 
 app.post("/urls/create", (req, res) => {
-  debugger;
-  if(req.cookies !== {}){
-    console.log("CREATE IF")
-    let username = req.cookies.username
-    const longURL = req.body.longURL;
-    const newshortURL = generateRandomString();
-    // urlDatabase[newshortURL] = longURL
+  if(req.session !== null){
+    let username = req.session.username
+    let longURL = req.body.longURL;
+    let newshortURL = generateRandomString();
 
     urlDatabase[newshortURL] = {url: longURL,
       username: username};
-    debugger;
     res.redirect("/urls");
   } else {
-    console.log("CREATE ELSE")
-    debugger;
     res.redirect("/login");
   }
 });
 
-// REDIRECT TO LONGURL BY USING SHORT
-app.get("/urls/:shortURL", (req, res) => {
-  debugger;
-  let templateVars = { urls: urlDatabase,
-     userid: req.cookies.userid };
-  let longURL = req.body.longURL;
-  res.render("urls_show", {shortURL: req.params.shortURL});
-});
+// //  ----------------------------------------  REDIRECT TO LONGURL BY USING SHORT
+// app.get("/urls/:shortURL", (req, res) => {
+//   let templateVars = { urls: urlDatabase,
+//      userid: req.session.user_id };
+//   let longURL = req.body.longURL;
+//   res.render("urls_show", {shortURL: req.params.shortURL});
+// });
 
-// LOGIN PAGE
-app.get("/login", (req, res) => {
-  debugger;
-    let templateVars = { urls: urlDatabase,
-      userid: ''
-    };
-  // username: req.signedCookies.username};
-  // var username = req.body.username;}
-  // var hash = bcrypt.hashSync('testing', 10);
-  // console.log(hash);
-  res.render("urls_index", templateVars);
-});
 
-// REDIRECT TO LONG URL BY ENTERING SHORTURL ID
+// ---------------------------------------- REDIRECT TO LONG URL BY ENTERING SHORTURL ID
 app.get("/u/:shortURL", (req, res) => {
-  debugger;
   let shortURL = req.params.shortURL;
-  let longURL = urlDatabase[shortURL];
-   debugger;
+  let longURL = urlDatabase[shortURL].url;
   res.redirect(longURL);
 });
 
-// CREATE NEW LONG URL
+// ---------------------------------------- CREATE NEW LONG URL
 app.post("/urls/:shortURL/update", (req, res) => {
-  debugger;
-  const newURL = (req.body.longURL);
-  const oldURL = urlDatabase[req.params.shortURL];
+  let newURL = req.body.longURL;
+  let oldURL = urlDatabase[req.params.shortURL];
   urlDatabase[req.params.shortURL] = newURL
-   debugger;
   res.redirect("/urls");
 });
 
-// DELETE URL by ID
+// ---------------------------------------- DELETE URL by ID
 app.post("/urls/:id/delete", (req, res) => {
-  debugger;
   let templateVars = { shortURL: req.params.id };
   delete urlDatabase[templateVars.shortURL];
-   debugger;
   res.redirect("/urls");
 });
 
-// CHECK IF LOGGED IN
+//  ---------------------------------------- LOGIN PAGE
+app.get("/login", (req, res) => {
+    let templateVars = { urls: urlDatabase,
+      userid: ''};
+  res.render("urls_index", templateVars);
+});
+
 app.post("/login", (req, res) => {
-  //If no users available:
-  // if(users = {}){
-  //     res.redirect(403, "/login")
-  // } else {
-  // // users is poplulate
-  debugger;
     for (let user in users) {
-      debugger;
-      // var inputPW = bcrypt.hashSync(req.body.password, 10);
-      // bcrypt.compareSync(req.body.password, user.password);
       if (req.body.email === users[user].email && bcrypt.compareSync(req.body.password, users[user].password)){
-        debugger;
-        res.cookie("userid", user)
+        req.session.user_id = users[user];
+        req.session.username = users[user].username;
         res.redirect("/");
       } else {
-        debugger;
         res.redirect(403, "/login")
       }
     }
-  // }
-  debugger;
 });
 
-// LOGOUT & DELETE COOKIE
+// ----------------------------------------LOGOUT & DELETE COOKIE
 app.post("/logout", (req, res) => {
-  const username = req.body.username
-  res.clearCookie('userid');
+  // let username = req.body.username
+  req.session = null;
   res.redirect('/login');
-  });
+});
 
-// REGISTRATION
+// --------------------------------------- REGISTRATION
 app.get("/register", (req, res) => {
   res.render("urls_register");
 });
@@ -191,9 +153,7 @@ app.post("/register", (req, res) => {
   var email = req.body.email;
   var password = bcrypt.hashSync(req.body.password, 10);
   var idNum = generateRandomString();
-  // var username1 = req.body.username;
 
-  //CHECK if
   if (username === "" || email === "" || password === "") {
     console.log("Please enter values for all fields.")
     res.redirect(400, "/register");
@@ -203,28 +163,21 @@ app.post("/register", (req, res) => {
       email: email,
       password: password
       }
+    users[idNum] = user;
 
-      users[idNum] = user;
-      //change all username cookies to userid
-      res.cookie("userid", idNum);
-      res.cookie("username", username);
-      res.redirect("/urls");
+    req.session.user_id = idNum;
+    req.session.username = username;
+    res.redirect("/urls");
   }
 
 });
 
-// app.use((req, res, next) => {
-//   if(!req.signedCookies.username) {
-//     res.redirect("/login")
-//   }
-//   next();
-// });
-
+// FOR USE BY USERID
 function generateRandomString() {
   var text = "";
   var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
     for( var i=0; i < 6; i++ ) {
-        text += possible.charAt(Math.floor(Math.random() * possible.length));
+      text += possible.charAt(Math.floor(Math.random() * possible.length));
     }
   return text;
 };
